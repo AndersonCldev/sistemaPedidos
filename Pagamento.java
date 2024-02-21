2405a5d202f0d565c4e76b2bb4e393fe3b5966a7
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2024-02-21 13:07:03 -0300

    Classe de pagamento
    A classe será responsável pelas funções de pagamento de pagamento do sistema, validação de escolhas e atribuição de valores do pedido do cliente.

---
+++ Pagamento.java
@@ -0,0 +1,57 @@
+package Pedido;
+
+public class Pagamento {
+
+	private Byte quantidadePedido; 
+	private Double subtotal;
+	private Double desconto;
+	private Double total; 
+	private Double[] valorProduto = {15.99, 9.97, 13.00};
+	
+			
+	//Métodos get e set quantidade
+	public byte getQuantidade() {
+		return quantidadePedido;
+	}
+	public void setQuantidade(byte quantidade) {
+		this.quantidadePedido = quantidade;
+	}
+	
+	//Métodos get e set subtotal
+	
+	public Double getSubtotal() {
+		return subtotal;
+	}
+	
+	public void setSubtotal(Double subtotal) {
+		this.subtotal = subtotal;
+	}
+	
+	//Métodos get e set desconto
+	public Double getDesconto() {
+		return desconto;
+	}
+	
+	public void setDesconto(Double desconto) {
+		this.desconto = 3*total / 10;
+	}
+	
+	
+	//Métodos get e set total
+	public Double getTotal() {
+		return total;
+	}
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+	
+	public Double[] getValorProduto() {
+		return valorProduto;
+	}
+	
+	public void setValorProduto( Double[] valorProduto) {
+		this.valorProduto = valorProduto;
+	}
+}
+	
+
